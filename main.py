a384a1b1ee5923f866538320b6072a3ee4c4352f
katheplazas <mariakatherineplazasolaya@gmail.com>
2022-10-03 17:18:46 -0500

    Ultimos Cambios

--- main.py
+++ main.py
@@ -11,8 +11,15 @@ from threading import Thread
 import preprocesamiento_service
 from sklearn import preprocessing
 import asyncio
+import py_eureka_client.eureka_client as eureka_client
 
 rest_port = 8050
+
+eureka_client.init(eureka_server="http://eureka:8761/eureka",
+                   app_name="preprocesamiento-seguridad",
+                   instance_port=rest_port)
+
+
 app = Flask(__name__)
 app.config["MONGO_URI"] = 'mongodb://root:123456@mongo:27018/preprocesamiento?authSource=admin'
 # app.config["MONGO_URI"] = 'mongodb://root:123456@mongo:27017/preprocesamiento?authSource=admin'
